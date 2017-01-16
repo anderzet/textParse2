18ab7a854943bbb064665abfd10111a70f42fe24
anderzet <andreybuchkovskiy@gmail.com>
2017-01-16 15:05:01 +0200

    Initial Commit

---
+++ textParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textParse
+//
+//  Created by Andrew on 16.01.17.
+//  Copyright © 2017 zetDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textParse
+//
+//  Created by Andrew on 16.01.17.
+//  Copyright © 2017 zetDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
